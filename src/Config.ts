cbaca2c206d37756576f7a4695a3cec4e3c4c279
Maxim Martynenko <starforce86714@gmail.com>
2021-09-03 12:22:37 -0700

    Add MS Oauth2

---
+++ src/Config.ts
@@ -0,0 +1,7 @@
+export const config = {
+    appId: '326b16bc-e55a-404a-b22d-9bdcd0b2aa61',
+    redirectUri: 'https://ms-oauth.herokuapp.com',
+    scopes: [
+        'Files.ReadWrite.All'
+    ]
+};
