e0c8f0d0af3ad901df6af32a19cb0285bb479c9c
Maxim Martynenko <starforce86714@gmail.com>
2021-09-03 12:53:22 -0700

    fix redirect url

--- src/Config.ts
+++ src/Config.ts
@@ -1,6 +1,6 @@
 export const config = {
     appId: '326b16bc-e55a-404a-b22d-9bdcd0b2aa61',
-    redirectUri: 'https://ms-oauth.herokuapp.com',
+    redirectUri: 'https://ms-outh2.netlify.app/',
     scopes: [
         'Files.ReadWrite.All'
     ]
